e62a6553971988d026a3675ba354b79eb21a4545
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-15 05:05:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# My Code here....
+def map_to_negativize(source_array)
+  counter = 0
+  new_array = []
+
+  while counter < source_array.length do
+    new_array << source_array[counter]*(-1)
+    counter += 1
+  end
+  return new_array
+end
+
+
